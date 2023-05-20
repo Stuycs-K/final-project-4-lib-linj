c37dcdabb4e2d22b7959837f0b2aa53ee49cc18d
Jason1165 <8333jlin@gmail.com>
2023-05-20 16:37:37 -0400

    Turn key into matrix

--- code/Hill.java
+++ code/Hill.java
@@ -3,17 +3,32 @@ import java.util.*;
 
 public class Hill {
   public static void main(String[] args) {
-    // System.out.println(readFile(args[0]));
+    encode(args[0], args[1]);
   }
 
   public static String encode(String fileToEncode, String keyFile) {
     String key = readFile(keyFile);
     String file = readFile(fileToEncode);
     StringBuilder encodedMessage = new StringBuilder();
-    // Matrix k = new Matrix();
-    /*
-        i += n where n is elements in matrix, varied based on size of key
-     */
+
+    Matrix k;
+    int l = key.length();
+    if ((int)Math.sqrt(l) * Math.sqrt(l) == l) {
+        double[][] mat = new double[(int)Math.sqrt(l)][(int)Math.sqrt(l)];
+        for (int i = 0; i < mat[0].length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                char c = key.charAt(i*3 + j);
+                mat[i][j] = (double)(c-65);
+            }
+        }
+        k = new Matrix(mat);
+        System.out.println(k.toString());
+    }
+    else {
+        System.out.println("Key is not sufficient to create an n by n matrix.");
+        System.exit(0);
+    }
+
     for (int i = 0; i < file.length(); i += 1) {
         // Matrix f = new Matrix();
         // convert to matrix

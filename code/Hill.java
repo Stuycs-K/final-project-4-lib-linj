4f48f9c8fd55fdb3cc5bc673936ef46d1ee0ef50
bli31 <bli31@stuy.edu>
2023-05-26 11:02:47 -0400

    KNOWN PLAINTEXT WORKS

--- code/Hill.java
+++ code/Hill.java
@@ -255,7 +255,7 @@ public class Hill {
     //  System.out.println("Original\n" + k);
      k = k.getInverse();
     //  System.out.println("Inverse\n" + k);
-     k = k.scalarMult(det);
+     k = k.scalarMult(Math.abs(det));
     //  System.out.println("Determinant: " + det);
     //  System.out.println("Scalar Mult\n" + k);
      int modular = getMultModInverse((int)det, 26);
